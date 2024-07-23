cca123c6321e265112016a700a91f9cb3fba6a3b
M40 <modev.404@gmail.com>
2024-07-23 11:40:57 +0300

    added prisma client

--- config/config.js
+++ config/config.js
@@ -1,6 +1,10 @@
 import Replicate from "replicate";
 import { config } from "dotenv";
+import { PrismaClient } from "@prisma/client";
 config()
+
 export const replicate = new Replicate({
     auth:process.env.REPLICATE_TOKEN,
 })
+
+export const prisma = new PrismaClient()
