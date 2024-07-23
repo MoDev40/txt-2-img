bbadbff44bf71b0baf75a36be87ccce12a810c2b
M40 <modev.404@gmail.com>
2024-07-23 10:40:36 +0300

    test replicate model

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-// const output = await replicate.run(
-//     "bytedance/sdxl-lightning-4step:5f24084160c9089501c1b3545d9be3c27883ae2239b6f412990e82d4a6210f8f",
-//     {
-//       input: {
-//         width: 1024,
-//         height: 1024,
-//         prompt: "a portrait girl of a rounded profile picture ",
-//         scheduler: "DDIM",
-//         num_outputs: 1,
-//         guidance_scale: 0,
-//         negative_prompt: "worst quality, low quality",
-//         num_inference_steps: 4
-//       }
-//     }
-//   );
-//   console.log(output);
+import cors from "cors";
+import { config } from "dotenv";
+import express from "express";
+import { replicate } from "./config/config.js";
+
+config()
+
+const app = express();
+
+app.use(cors())
+
+app.get("/",async(req,res)=>{
+    const output = await replicate.run(
+        "bytedance/sdxl-lightning-4step:5f24084160c9089501c1b3545d9be3c27883ae2239b6f412990e82d4a6210f8f",
+        {
+      input: {
+        width: 1024,
+        height: 1024,
+        prompt: "a portrait man of a rounded profile picture ",
+        scheduler: "DDIM",
+        num_outputs: 1,
+        guidance_scale: 0,
+        negative_prompt: "worst quality, low quality",
+        num_inference_steps: 4
+      }
+    }
+  );
+    res.send(output)
+})
+
+
+app.listen(8000,()=>{
+    console.log("connected")
+})
